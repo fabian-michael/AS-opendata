4e247b282cd304368412125ba16f97c716d48912
Mycl95 <fabian.michael95@gmail.com>
2020-05-12 12:13:21 +0200

    init and adapt boilerplate

---
+++ src/App/index.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Layout from '@Components/Layout';
+    import ImageExample from '@Components/ImageExample';
+    import HelloWorld from '@Components/HelloWorld';
+</script>
+
+<style lang="scss">
+    .container {
+        background-color: #2c3e50;
+        display: flex;
+        flex-direction: column;
+        flex: 1 1 100%;
+        justify-content: flex-start;
+        align-items: center;
+        min-height: 100vh;
+
+        &__top {
+            margin: 100px;
+        }
+
+        &__middle {
+            display: flex;
+            flex: 1 1 100%;
+            justify-content: center;
+            align-items: flex-start;
+        }
+    }
+</style>
+
+<Layout>
+    <div class="container">
+        <div class="container__top">
+            <ImageExample />
+        </div>
+        <div class="container__middle">
+            <HelloWorld />
+        </div>
+    </div>
+</Layout>
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,22 @@ const path = require('path');
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
+const sveltePreprocess = require('svelte-preprocess');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isInline = process.env.inline;
+
 module.exports = {
 	entry: {
 		bundle: ['./src/main.js']
 	},
 	resolve: {
 		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
+			svelte: path.resolve('node_modules', 'svelte'),
+			'@Components': path.resolve(__dirname, 'src/Components/'),
+			'@Services': path.resolve(__dirname, 'src/Services/'),
+			'@Assets': path.resolve(__dirname, 'src/Assets/'),
 		},
 		extensions: ['.mjs', '.js', '.svelte'],
 		mainFields: ['svelte', 'browser', 'module', 'main']
@@ -23,33 +32,68 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.(js|mjs|svelte)$/,
+				exclude: /node_modules\/(?!svelte)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-proposal-object-rest-spread']
+					}
+				},
+			},
+			{
 				test: /\.svelte$/,
+				exclude: /node_modules/,
 				use: {
 					loader: 'svelte-loader',
 					options: {
 						emitCss: true,
-						hotReload: true
+						hotReload: true,
+						preprocess: sveltePreprocess({
+							scss: {
+								renderSync: true
+							}
+						})
 					}
 				}
 			},
 			{
 				test: /\.css$/,
 				use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'css-loader'
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: !prod
+						}
+					},
+					'css-loader',
+					'sass-loader'
 				]
-			}
+			},
+			{
+				test: /\.(png|jpe?g|gif|mp4|svg|ttf)$/,
+				use: [
+					{
+						loader: 'file-loader'
+					},
+				],
+			},
 		]
 	},
 	mode,
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: '[name].css'
-		})
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			title: 'svelte-boilerplate'
+		}),
 	],
-	devtool: prod ? false: 'source-map'
+	devtool: prod ? false : 'source-map',
+	devServer: {
+		port: 8080,
+		writeToDisk: true
+	}
 };
