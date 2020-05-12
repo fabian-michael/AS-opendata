d7eac45d71814c1519e2f3521cdc0d5c1b46140d
Mycl95 <fabian.michael95@gmail.com>
2020-05-12 13:51:32 +0200

    remove files

--- src/App/index.svelte
+++ src/App/index.svelte
@@ -1,39 +1,14 @@
 <script>
     import Layout from "@Components/Layout";
-    import ImageExample from "@Components/ImageExample";
-    import HelloWorld from "@Components/HelloWorld";
+    import FetchExample from "@Components/FetchExample";
 </script>
 
 <style lang="scss">
-    .container {
-        background-color: #2c3e50;
-        display: flex;
-        flex-direction: column;
-        flex: 1 1 100%;
-        justify-content: flex-start;
-        align-items: center;
-        min-height: 100vh;
 
-        &__top {
-            margin: 100px;
-        }
-
-        &__middle {
-            display: flex;
-            flex: 1 1 100%;
-            justify-content: center;
-            align-items: flex-start;
-        }
-    }
 </style>
 
 <Layout>
     <div class="container">
-        <div class="container__top">
-            <ImageExample />
-        </div>
-        <div class="container__middle">
-            <HelloWorld />
-        </div>
+        <FetchExample />
     </div>
 </Layout>
